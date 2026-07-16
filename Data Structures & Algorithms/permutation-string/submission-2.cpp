dcb9df4c706b74d478c49565f98966011264766a
DarkboyRyan <76423101+DarkboyRyan@users.noreply.github.com>
2026-07-16 06:37:03 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> hashM;
+
+        for(char s : s1){
+            hashM[s]++;
+        }
+        int l = hashM.size();
+        for(int i = 0; i < s2.size(); i++){
+            unordered_map<char,int> hashM2;
+            int cur = 0;
+            for(int j = i; j < s2.size();j++){
+                char c = s2[j];
+                hashM2[c]++;
+
+                if(hashM[c] < hashM2[c]){
+                    break;
+                }
+                if(hashM[c] == hashM2[c]){
+                    cur++;
+                }
+                if(cur == l){
+                    return true;
+                }
+            }           
+        }
+        return false;     
+    }
+};
